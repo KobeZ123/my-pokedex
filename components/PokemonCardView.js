c25e186eecfcade41717b7c736afb149fefe5f5c
KobeZ123 <kobezhe@gmail.com>
2023-04-29 15:37:11 -0400

    pokemon card view design completed

--- components/PokemonCardView.js
+++ components/PokemonCardView.js
@@ -31,21 +31,56 @@ export default function PokemonCardView(props) {
         </View>: 
         <View style={styles.container}>
             <Image style={styles.image} source={{uri: (data.sprites.other["official-artwork"].front_default)}}/> 
-            <Text>DONE LOADING</Text>
+            <View style={styles.text_info_container}>
+                <Text style={[styles.text, styles.name_text]}>{data.name}</Text>
+                <View style={styles.index_info_container}>
+                    <Text style={[styles.text, styles.index_hashtag]}>#</Text>
+                    <Text style={[styles.text, styles.index_value]}>{data.game_indices[0].game_index}</Text>
+                </View>
+                
+            </View>
+            
         </View>
-
-        
     );
     
 }
 
 const styles = StyleSheet.create({
     container: {
-        display: "flex"
+        display: "flex",
+        width: 200,
+        paddingBottom: 8,
+        paddingHorizontal: 5,
     }, 
+    text: {
+        fontFamily: "Orbitron",
+    },
     image: {
-        width: '20%',
         aspectRatio: 1/1,
-        // mixBlendMode: "multiply"
+        mixBlendMode: "multiply",
+        backgroundColor: "gainsboro",
+        borderRadius: 5,
+    },
+    text_info_container: {
+        paddingVertical: 5,
+        paddingLeft: 10, 
+    },
+    name_text: {
+        fontWeight: "600",
+        fontSize: 24,
+    }, 
+    index_info_container: {
+        display: "flex",
+        flexDirection: "row",
+    }, 
+    index_hashtag: {
+        color: "dimgrey",
+        fontWeight: "600",
+        fontSize: 18,
+    },
+    index_value: {
+        color: "dimgrey",
+        fontWeight: "600",
+        fontSize: 18,
     },
 }); 
